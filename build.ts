3210f490c370519a70cc648b3faa3cb14254dbfc
Katsute <58778985+Katsute@users.noreply.github.com>
2021-12-31 13:40:24 -0500

    set up project

---
+++ build.ts
@@ -0,0 +1,32 @@
+// Copyright (C) Katsute 2022
+
+import { https } from 'follow-redirects';
+import fs, { WriteStream } from "fs";
+import path from "path";
+
+const version: string = "v3.1.0";
+
+class Main {
+
+    public static async main(): Promise<void> {
+        const layouts: string = path.join(__dirname, "layouts");
+
+        fs.existsSync(layouts) || fs.mkdirSync(layouts);
+
+        const file: WriteStream = fs.createWriteStream(path.join(layouts, "compress.html"));
+        https.get(`https://github.com/penibelst/jekyll-compress-html/releases/download/$[version}/compress.html`, (response) => {
+            response.pipe(file);
+        });
+    }
+
+}
+
+process.on("unhandledRejection", (error: Error, promise: any) => {
+    console.error(`Unhandled rejection at:\n  Promise ${promise}\n  ${error.stack}`);
+    process.exit(-1);
+});
+
+Main.main().catch((error: Error) => {
+    console.error(error.stack!);
+    process.exit(-1);
+});
